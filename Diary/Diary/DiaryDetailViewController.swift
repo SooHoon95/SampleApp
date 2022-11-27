3b1e39f234b222b41beb00302d948189e87788bd
Hyeon <81559117+SooHoon95@users.noreply.github.com>
2022-11-27 12:29:33 +0900

    상세화면 작성

---
+++ Diary/Diary/DiaryDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by 최수훈 on 2022/11/27.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
