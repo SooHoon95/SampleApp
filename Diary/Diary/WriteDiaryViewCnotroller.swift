ae2991fdc98e72cffd456d116194a980e23fb189
Hyeon <81559117+SooHoon95@users.noreply.github.com>
2022-11-27 12:24:28 +0900

    WriteDiartViewController

---
+++ Diary/Diary/WriteDiaryViewCnotroller.swift
@@ -0,0 +1,19 @@
+//
+//  WriteDiaryViewCnotroller.swift
+//  Diary
+//
+//  Created by 최수훈 on 2022/11/27.
+//
+
+import UIKit
+
+class WriteDiaryViewCnotroller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
