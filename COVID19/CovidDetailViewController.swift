6b8082167e943257029a229884f796b74b57a1f4
Hyeon <81559117+SooHoon95@users.noreply.github.com>
2022-12-20 14:34:57 +0900

    UI구성

---
+++ COVID19/CovidDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CovidDetailViewController.swift
+//  COVID19
+//
+//  Created by 최수훈 on 2022/12/20.
+//
+
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+
+    @IBOutlet weak var newCaseCell: UITableViewCell!
+    @IBOutlet weak var totlaCaseCell: UITableViewCell!
+    @IBOutlet weak var recoverdCell: UITableViewCell!
+    @IBOutlet weak var deathCell: UITableViewCell!
+    @IBOutlet weak var percentageCell: UITableViewCell!
+    @IBOutlet weak var overseasInflowCell: UITableViewCell!
+    @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
